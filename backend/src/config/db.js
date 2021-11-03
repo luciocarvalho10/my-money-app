d4e508f6f2057b8950de7c87909a43691248e2a9
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-03 18:04:40 -0400

    configurando error messages

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/mymoney')
+
+mongoose.Error.messages.general.required = "O atributo {PATH} é obrigatório!"
+mongoose.Error.messages.Number.min = "O {VALUE} informado é menor que o pertminito - {MIN}!"
+mongoose.Error.messages.Number.max = "O {VALUE} informado é maior que o pertminito - {MAN}!"
+mongoose.Error.messages.String.enum = "O {VALUE} não e válido para o atributo {PATH}!"
