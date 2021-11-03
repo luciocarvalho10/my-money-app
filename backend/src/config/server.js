6c7c75be083f616d631bb3933bc0d00a8b2f4d94
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-03 19:54:55 -0400

    habilitando cors

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -4,9 +4,11 @@ const backlog = () => console.log(`BACKEND is runnig at ${port} - ${new Date()}`
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const cors = require('./cors')
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(cors)
 
 server.listen(port, backlog)
 
