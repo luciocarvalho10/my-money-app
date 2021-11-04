6a49708d2520906996eef4c8cd149c629851da63
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-04 00:05:52 -0400

    componentes Index e App

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,19 +2,19 @@ const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
-  entry: '.src/index.jsx',
+  entry: './src/index.jsx',
   output: {
     path: __dirname + '/public',
     filename: './app.js'
   },
   devServer: {
     port: 8080,
-    contentBase: '/.public'
+    contentBase: './public'
   },
   resolve: {
-    extensions: ['', 'js', 'jsx'],
+    extensions: ['', '.js', '.jsx'],
     alias: {
-      modules: __dirname + 'node_modules',
+      modules: __dirname + '/node_modules',
       jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
       bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
     }
@@ -32,7 +32,7 @@ module.exports = {
       {
         test: /.js[x]?$/,
         loader: 'babel-loader',
-        exclude: /node_module/,
+        exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react'],
           plugins: ['transform-object-rest-spread']
