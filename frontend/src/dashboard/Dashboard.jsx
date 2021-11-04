7f0dc0735f27d3a7f5b799b215cd1210915d8264
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-04 18:59:49 -0400

    componente titulo do conteudo

--- frontend/src/dashboard/Dashboard.jsx
+++ frontend/src/dashboard/Dashboard.jsx
@@ -1,7 +1,21 @@
-import React from "react"
+import React, { Component } from "react"
+import Content from "../common/template/Content"
+import ContentHeader from "../common/template/ContentHeader"
 
-export default props => (
-  <div>
-    <h1>Dashboard</h1>
-  </div>
-)
+class Dashboard extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>
+        <ContentHeader title="Dashboard" sub="Versão 1.0" />
+        <Content>
+          <h1>Dashboard</h1>
+        </Content>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
