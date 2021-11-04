4203cfe37bea66d58dd13c16e05dad170beea7fd
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-04 17:13:10 -0400

    componente rodape

---
+++ frontend/src/common/template/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2021{' '}
+      <a href="https://github.com/luciocarvalho10" target="_blank">
+        LUCIOCARVALHO10 <i className="fa fa-github"></i>
+      </a>
+    </strong>
+  </footer>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -2,10 +2,13 @@ import '../common/template/dependencies'
 import React from "react"
 import Header from '../common/template/Header'
 import SideBar from '../common/template/SideBar'
+import Footer from '../common/template/Footer'
 
 export default props => (
   <div className="wrapper">
     <Header />
     <SideBar />
+    <div className="content-wrapper"><h1>Conteúdo</h1></div>
+    <Footer />
   </div>
 )
