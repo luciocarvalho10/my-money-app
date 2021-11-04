be515eda28b88a1174b067d00fca50dcca918af7
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-04 16:52:28 -0400

    componente menu

---
+++ frontend/src/common/template/Menu.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import MenuItem from "./MenuItem"
+import MenuTree from "./MenuTree"
+
+
+export default props => (
+  <ul className="sidebar-menu">
+    <MenuItem path='#' icon='dashboard' label='Dashboard'/>
+    <MenuTree label='Cadastro' icon='edit'>
+      <MenuItem path='#billingCycles' icon='usd' label='Ciclo de Pagamentos'/>
+    </MenuTree>
+  </ul>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,9 +1,11 @@
 import '../common/template/dependencies'
 import React from "react"
 import Header from '../common/template/Header'
+import SideBar from '../common/template/SideBar'
 
 export default props => (
   <div className="wrapper">
     <Header />
+    <SideBar />
   </div>
 )
