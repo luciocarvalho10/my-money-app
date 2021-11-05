f81f9f8e340bbfa3f4a48f475371e82cbed8ae01
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-05 13:41:34 -0400

    configurando redux

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,8 +1,13 @@
 import React from "react"
 import ReactDom from 'react-dom'
+import {Provider} from 'react-redux'
+
 import App from "./main/app"
+import store from "./main/store"
 
 ReactDom.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('app')
 )
