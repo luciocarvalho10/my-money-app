6a0f85329071ca7da4e49d6c8545b4f36cb40d4b
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-05 15:42:10 -0400

    aplicando middlewares para request e devtools

--- frontend/src/main/store.js
+++ frontend/src/main/store.js
@@ -1,7 +1,9 @@
-import {createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
+import promise from 'redux-promise'
 
 import reducers from './reducers'
 
-const store = createStore(reducers)
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = applyMiddleware(promise)(createStore)(reducers, devtools)
 
 export default store
