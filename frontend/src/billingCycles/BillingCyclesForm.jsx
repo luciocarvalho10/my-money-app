30cde7f07f11c4f322cb9353372464662a685110
Lucio <luciocarvalho10@hotmail.com.br>
2021-11-10 13:09:57 -0400

    personalizando Componente Field

--- frontend/src/billingCycles/BillingCyclesForm.jsx
+++ frontend/src/billingCycles/BillingCyclesForm.jsx
@@ -1,22 +1,45 @@
-import React, { Component } from "react"
-import {reduxForm, Field} from 'redux-form'
+import React, { Component } from 'react'
+import { reduxForm, Field } from 'redux-form'
+import InputAndLabel from '../common/form/InputAndLabel'
 
 class BillingCyclesForm extends Component {
   render() {
-    const {handleSubmit} = this.props
+    const { handleSubmit } = this.props
     return (
       <form role="form" onSubmit={handleSubmit}>
         <div className="box-body">
-          <Field name='name' component='input' />
-          <Field name='month' component='input' />
-          <Field name='year' component='input' />
+          <Field
+            name="name"
+            component={InputAndLabel}
+            label="Nome"
+            cols="12 4"
+            placeholder="Informe o nome"
+          />
+          <Field
+            name="month"
+            component={InputAndLabel}
+            type="Number"
+            label="Mês"
+            cols="12 4"
+            placeholder="Informe o mês"
+          />
+          <Field
+            name="year"
+            component={InputAndLabel}
+            type="Number"
+            label="Ano"
+            cols="12 4"
+            placeholder="Informe o ano"
+          />
         </div>
         <div className="box-footer">
-          <button type="submit" className='btn btn-primary'>Submit</button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </div>
       </form>
     )
   }
 }
 
-export default reduxForm({form: 'BillingCyclesForm'})(BillingCyclesForm)
+export default reduxForm({ form: 'BillingCyclesForm' })(BillingCyclesForm)
