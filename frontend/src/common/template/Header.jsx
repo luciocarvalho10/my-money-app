09c6e0c31942b2bedc555b849e121db2e5176ea5
luciocarvalho10 <luciocarvalho10@hotmail.com.br>
2022-01-02 13:25:05 -0400

    criando componente NavBar

--- frontend/src/common/template/Header.jsx
+++ frontend/src/common/template/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import NavBar from "./NavBar"
 
 export default props => (
   <header className="main-header">
@@ -13,6 +14,7 @@ export default props => (
     </a>
     <nav className="navbar navbar-static-top">
       <a href className="sidebar-toggle" data-toggle="offcanvas"></a>
+      <NavBar />
     </nav>
   </header>
 )
